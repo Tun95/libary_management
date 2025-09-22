ef442c94fa088d7e8b2a03c0cc9add5c20d46c66
Tunji Akande <akandetunji3@gmail.com>
2025-09-22 11:56:38 +0100

    FEAT: update config

--- backend_service/config/index.js
+++ backend_service/config/index.js
@@ -16,6 +16,34 @@ module.exports = {
     endpoint: process.env.LOGTAIL_ENDPOINT || "your_logtail_endpoint_here",
   },
 
-  providers: {},
+  providers: {
+    paystack: {
+      provider: process.env.PAYSTACK_PROVIDER || "paystack",
+      authConfig: {
+        development: {
+          secretKey:
+            process.env.PAYSTACK_SECRET_KEY ||
+            "sk_test_your_paystack_test_secret_key",
+          publicKey:
+            process.env.PAYSTACK_PUBLIC_KEY ||
+            "pk_test_your_paystack_test_public_key",
+        },
+        production: {
+          secretKey:
+            process.env.PAYSTACK_SECRET_KEY ||
+            "sk_live_your_paystack_live_secret_key",
+          publicKey:
+            process.env.PAYSTACK_PUBLIC_KEY ||
+            "pk_live_your_paystack_live_public_key",
+        },
+      },
+    },
+
+    email: {
+      service: process.env.EMAIL_SERVICE || "gmail",
+      username: process.env.EMAIL_USERNAME || "your_email@gmail.com",
+      password: process.env.EMAIL_PASSWORD || "your_email_password",
+    },
+  },
   defaultProvider: {},
 };
